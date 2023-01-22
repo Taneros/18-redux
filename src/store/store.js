60e0a98ecd5cafc073ee4670ba503201054c869c
Taneros <fricentaurus@GMAIL.COM>
2023-01-22 13:20:19 +0300

    feat: use redux to increment decrement

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 import {createStore} from 'redux';
 
 const counterReducer = (state = {counter: 0}, action) => {
-  if (action.type === 'increment') return {counter: state.counter++};
-  if (action.type === 'decrement') return {counter: state.counter--};
+  if (action.type === 'increment') return {counter: ++state.counter};
+  if (action.type === 'decrement') return {counter: --state.counter};
   return {...state};
 };
 
 const store = createStore(counterReducer);
 
-export default store
+export default store;
